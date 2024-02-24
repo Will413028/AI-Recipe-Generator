d033c15ef99ac2a736f977394f434af0e0010614
will <will413028@gmail.com>
2024-02-24 13:38:35 +0800

    feat: add gen recipe

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,14 @@ import numpy as np
 import math
 import streamlit as st
 from PIL import Image
+from openai import OpenAI
 
 
 api_key = st.secrets["OPENAI_API_KEY"]
 
 classNames = ['avocado', 'beans', 'beet', 'bell pepper', 'broccoli', 'brus capusta', 'cabbage', 'carrot', 'cayliflower', 'celery', 'corn', 'cucumber', 'eggplant', 'fasol', 'garlic', 'hot pepper', 'onion', 'peas', 'potato', 'pumpkin', 'rediska', 'redka', 'salad', 'squash-patisson', 'tomato', 'vegetable marrow']
+
+client = OpenAI(api_key=api_key)
 
 def process_image_and_generate_recipe(image_data):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -33,13 +36,34 @@ def process_image_and_generate_recipe(image_data):
             cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
             cv2.putText(frame, label, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255,255,255), 2)
 
-    return frame
+    if class_final_names:
+        ingredients_detected = ','.join(np.unique(class_final_names))
+    
+    return frame, ingredients_detected
+
+def generate_recipe(ingredients):
+    try:
+        response = client.chat.completions.create(model="gpt-3.5-turbo",
+                                                  messages=[
+                                                      {"role": "system", "content": "Take these ingredients" + ingredients},
+                                                      {"role": "system", "content": "Generate a Recipe Based on these Ingredients."},
+                                                      {"role": "system", "content": "Please write in Traditional Chinese language."}
+                                                  ])
+        result = ''
+        for choice in response.choices:
+            result += choice.message.content
+        return result
+    except Exception as e:
+        print(f"生成食譜時發生錯誤: {e}")
+        return "無法生成食譜，請稍後再試。"
 
 
 st.set_page_config(page_title="蔬食智能食譜", page_icon="📸")
 st.title("蔬食智能食譜")
-st.title(api_key)
+
 uploaded_file = st.file_uploader("請上傳一張照片或使用手機拍照", type=["jpg", "jpeg", "png"])
+
+st.title("您的食譜將在此生成")
 
 if uploaded_file is not None:
     
@@ -47,7 +71,8 @@ if uploaded_file is not None:
     
     st.image(image, caption="上傳的照片", use_column_width=True)
 
-    processed_image = process_image_and_generate_recipe(image)
+    processed_image, ingredients_detected = process_image_and_generate_recipe(image)
     processed_image = cv2.cvtColor(processed_image, cv2.COLOR_BGR2RGB)
 
-    st.image(processed_image, caption="預測結果", use_column_width=True)
+    article = generate_recipe(ingredients_detected)
+    st.write(article)

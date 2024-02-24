441e2ed7799d0f55f5b802f64b045bedf19b7240
will <will413028@gmail.com>
2024-02-25 05:21:21 +0800

    refactor: improve readability

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,17 @@ def generate_recipe(ingredients, cuisine):
             return "無法生成食譜，請稍後再試。"
 
 
+def display_image_and_detect_ingredients(uploaded_file, model):
+    if uploaded_file is not None:
+        with st.spinner('圖片處理中...'):
+            image = Image.open(uploaded_file)
+            st.image(image, width=300)
+            processed_image, ingredients_detected = process_image(image, model)
+            processed_image = cv2.cvtColor(processed_image, cv2.COLOR_BGR2RGB)
+            return ingredients_detected
+    return []
+
+
 def main():
     st.title("蔬食智能食譜")
     with st.spinner('模型載入中，請稍候...'):
@@ -39,22 +50,16 @@ def main():
     uploaded_file = st.file_uploader("請上傳一張照片或使用手機拍照", type=["jpg", "jpeg", "png"])
     cuisine = st.selectbox("選擇料理種類", ["台灣料理", "日本料理"])
 
-    if uploaded_file is None:
-        st.title("上傳照片後，您的食譜將在此生成")
-
-    if uploaded_file is not None:
-        with st.spinner('圖片處理中...'):
-            image = Image.open(uploaded_file)
-            st.image(image, width=300)
-
-        processed_image, ingredients_detected = process_image(image, model)
-        processed_image = cv2.cvtColor(processed_image, cv2.COLOR_BGR2RGB)
-
+    if uploaded_file:
+        ingredients_detected = display_image_and_detect_ingredients(uploaded_file, model)
         if ingredients_detected:
             recipe = generate_recipe(ingredients_detected, cuisine)
             st.write(recipe)
         else:
             st.write("沒有辨識到任何食物，請嘗試其他照片")
+    else:
+        st.markdown("<p style='font-size: 16px;'>上傳照片後，您的食譜將在此生成</p>", unsafe_allow_html=True)
+
 
 if __name__ == "__main__":
     main()
